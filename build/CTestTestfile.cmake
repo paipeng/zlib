453127af06c464a1e299f1c71652d7d8c9d875a8
Pai Peng <sipaipv6@gmail.com>
2022-05-02 12:02:25 +0800

    cmake vs2019

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/paipeng/git/zlib
+# Build directory: C:/Users/paipeng/git/zlib/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(example "example")
+set_tests_properties(example PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/paipeng/git/zlib/CMakeLists.txt;235;add_test;C:/Users/paipeng/git/zlib/CMakeLists.txt;0;")
